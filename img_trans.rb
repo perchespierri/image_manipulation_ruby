8e3e37526cf32489b175b9d067f9f0234d1ff432
perchespierri <perchespierri@gmail.com>
2022-01-11 13:35:57 -0300

    Implement watermark function

--- img_trans.rb
+++ img_trans.rb
@@ -1,6 +1,17 @@
 require 'mini_magick'
 
-image = MiniMagick::Image.open('exp.jpg')
-image.colospace 'gray'
-image.write('output.jpg')
+image = MiniMagick::Image.open(ARGV[0])
 
+ARGV.each do |arg|
+  case
+    when arg == 'to_black_and_white'
+      image.colorspace 'gray'
+    when arg.include?('resize')
+      image.resize arg.split('=')[1]
+    when arg.include?('watermark')
+      image.combine_options do |c|
+        c.gravity 'SouthEast'
+        c.draw 'image Over 0,0 0,0 #{arg.split('=')[1]}'
+    end
+  end
+image.write('output.jpg')
